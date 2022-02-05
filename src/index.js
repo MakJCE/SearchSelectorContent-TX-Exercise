5ff2e85c710b5b1e2a26912d139dfa9b99023c84
MakJhonny <makykamacho@gmail.com>
2022-02-05 02:29:03 -0400

    setup App and index files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#00FFF5",
+    },
+    secondary: {
+      main: "#393E46",
+    },
+    textPrimary: {
+      main: "#FFFFFF",
+    },
+    textSecondary: {
+      main: "#000000",
+    },
+  },
+  typography: {
+    fontFamily: "Segoe UI",
+    fontSize: 18,
+    fontWeight: 'normal',
+  },
+});
+
+theme.typography.h1 = {
+  fontFamily: "Segoe UI",
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '30px',
+  '@media (min-width:768px)': {
+    fontFamily: "Segoe UI",
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '42px',
+    lineHeight: '55px',
+  },
+}
+theme.typography.h2 = {
+  fontFamily: "Segoe UI",
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  fontSize: '26px',
+  '@media (min-width:768px)': {
+    fontFamily: "Segoe UI",
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '36px',
+    lineHeight: '55px',
+  },
+}
+theme.typography.h3 = {
+  fontFamily: "Segoe UI",
+  fontStyle: 'normal',
+  fontWeight: '400px',
+  fontSize: '18px',
+  '@media (min-width:768px)': {
+    fontFamily: "Segoe UI",
+    fontStyle: 'normal',
+    fontWeight: '400px',
+    fontSize: '27px',
+    lineHeight: '55px',
+  },
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
